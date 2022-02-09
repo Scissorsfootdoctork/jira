e8462cfea9ac7a04f895475612fb6fdf6193c62b
Scissorsfootdoctork <scissorwz@gmail.com>
2022-02-09 17:34:46 +0800

    安装emotion 优化登录状态界面

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,4 +1,5 @@
 import { User } from "./search-panel";
+import { Table } from "antd";
 
 interface Project {
   id: string;
@@ -14,20 +15,14 @@ interface ListProps {
 }
 
 export const List = ({ list, users }: ListProps) => {
-  return <table>
-    <thead>
-    <tr>
-      <th>名称</th>
-      <th>负责人</th>
-    </tr>
-    </thead>
-    <tbody>
+  return <Table pagination={false} dataSource={list} columns={[
+    { title: "名称", dataIndex: "name", sorter: ((a, b) => a.name.localeCompare(b.name)) },
     {
-      list.map(project => <tr key={project.id}>
-        <td>{project.name}</td>
-        <td>{users.find(user => project.personId === user.id)?.name || "未知"}</td>
-      </tr>)
+      title: "负责人", render(value, project) {
+        return <span>
+        {users.find((user) => user.id === project.personId)?.name || "未知"}
+      </span>;
+      }
     }
-    </tbody>
-  </table>;
+  ]} />;
 };
